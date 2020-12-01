65cbc530012757f0d0df59e4513877cb0ffd5d44
rlsurra <rlsurra@hotmail.com>
2020-12-01 14:43:22 -0300

    Header + Container

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
-import React from 'react';
+import React, {Fragment} from 'react';
+import Header from './components/Header';
 
 function App() {
   return (
-    <h1>Noticias</h1>
+    <Fragment>
+      <Header titulo="Buscador de noticias"/>
+      <div className="container whit">
+        <h1>¡Acá va el form!</h1>
+      </div>
+    </Fragment>
   );
 }
 
